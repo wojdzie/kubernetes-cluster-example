4db9c8e2c3939b9e522dc56135e1e83feff03d9b
wojdzie <wojdzi@outlook.com>
2023-04-04 19:56:40 +0200

    New Account Signup Service init

---
+++ new-account-signup-service/src/main/java/com/github/wojdzie/nass/NewAccountSignupServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.wojdzie.nass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAccountSignupServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewAccountSignupServiceApplication.class);
+    }
+}
